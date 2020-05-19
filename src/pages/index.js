68eb937712067fac45c6bde224e715304ae2e221
Rhys Dent <rhyswdent@gmail.com>
2020-05-19 08:32:32 -0700

    init commit and added data/images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,41 @@
 import React from "react"
+import team from "../data/theTeam"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    Miles Davison
+    <ul>
+      {team.map(member => (
+        <li>
+          <div>{member.name}</div>
+          <div>{member.position}</div>
+          <ul>
+            {member.photoURLs.map(url => (
+              <li>{url}</li>
+            ))}
+          </ul>
+          <ul>
+            {member.practice.map(prac => (
+              <li>{prac}</li>
+            ))}
+          </ul>
+          <ul>
+            {member.bio.map(b => (
+              <li>{b}</li>
+            ))}
+          </ul>
+          <ul>
+            {member.education.map(educ => (
+              <li>{educ}</li>
+            ))}
+          </ul>
+          <ul>
+            {member.experience.map(exp => (
+              <li>{exp}</li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+  </div>
+)
